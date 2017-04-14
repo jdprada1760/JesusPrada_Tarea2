da82f98f64a21021ec70e41121f73402d3e9e88a
jdprada1760 <jd.prada1760@uniandes.edu.co>
2017-04-14 16:52:39 -0500

    More preliminaries

--- SedovExplosion/Sedov.c
+++ SedovExplosion/Sedov.c
@@ -37,25 +37,32 @@ FLOAT blast = pow(10.0,10.0);
 // Gas temperature in Kelvin
 FLOAT T = 300.0;
 // Gas Pressure in Pascals
-FLOAT P = 1.0*atm; 
+FLOAT P = 1.0*atm;
+// Velocity v
+// Pressure p, internal energy e, density rho
+FLOAT** v;
+FLOAT* p,e,rho;
 
 
+//------------------------------------------------------------------------------
+//   MAIN
+//------------------------------------------------------------------------------
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+int main(int argc, char** argv){
+	
+	// N is defined 
+	N = ((FLOAT)NN)/((FLOAT)resol);
+	// Initialises variables
+	v = malloc(3*sizeof(float));
+	allocate3d(v[0]);
+	allocate3d(v[1]);
+	allocate3d(v[2]);
+	allocate3d(p);
+	allocate3d(rho);
+	allocate3d(e);
+	return 0;
+}
 
 
 
@@ -72,7 +79,7 @@ FLOAT P = 1.0*atm;
  *
  */
 void allocate3d(FLOAT* arr){
-  arr = malloc(N*N*N*sizeof(FLOAT));
+	arr = malloc(N*N*N*sizeof(FLOAT));
 }
 
 /*
@@ -81,8 +88,73 @@ void allocate3d(FLOAT* arr){
  *
  */
 void index(int i, int j, int k){
-  return N*N*i + N*j + k;
+	return N*N*i + N*j + k;
 }
+
+
+/*
+ * Initializes variables according to the corresponding initial conditions
+ *
+ */
+void __init__(){
+	int i;
+	int j;
+	int k;
+	// Fills variables as if it was a normal ideal gas
+	for( i = 0; i < N; i++ ){
+		for( j = 0; j < N; j++ ){
+			for( k = 0; k < N; k++ ){
+				p[index(i,j,k)] = P;
+				v[0][index(i,j,k)] = 0.0;
+				v[1][index(i,j,k)] = 0.0;
+`				v[2][index(i,j,k)] = 0.0;
+				e[index(i,j,k)] = stateEq(v[0][index(i,j,k)],v[1][index(i,j,k)],v[2][index(i,j,k)],p[index(i,j,k)]);
+				rho[index(i,j,k)] = getRho(v[0][index(i,j,k)],v[1][index(i,j,k)],v[2][index(i,j,k)],p[index(i,j,k)]);
+			}
+		}
+	}
+	// Sedovs blast energy is corrected (4m side)
+	e[index(N/2,j,k)]     = blast;
+	e[index(N/2 - 1,j,k)] = blast;
+	e[index(i,N/2,k)]     = blast;
+	e[index(i,N/2 - 1,k)] = blast;
+	e[index(i,j,N/2)]     = blast;
+	e[index(i,j,N/2 - 1)] = blast;
+}
+
+
+/*
+ * Gets energy in terms of other variables (pressure p, velocity v, density rho)
+ *
+ */
+FLOAT stateEq(FLOAT vx, FLOAT vy, FLOAT vz, FLOAT p, FLOAT rho){
+	// Which is the state equation?
+	return 0;
+}
+
+/*
+ * Gets rho density in terms of other variables (pressure p, velocity v, density rho)
+ *
+ */
+FLOAT getRho(FLOAT vx, FLOAT vy, FLOAT vz, FLOAT p, FLOAT rho){
+	// How to calculate rho?
+	return 0;
+}
+
+/*
+ * Gets sound speed in terms of other variables (pressure p, velocity v, density rho)
+ *
+ */
+FLOAT getSoundSpeed(FLOAT vx, FLOAT vy, FLOAT vz, FLOAT p, FLOAT rho){
+	// How to calculate sound speed?
+	return 0;
+}
+
+
+
+
+
+
 
 
 
