dcbcbb97ad0527f011242d2732cfc59fa511114a
jdprada1760 <jd.prada1760@uniandes.edu.co>
2017-04-25 15:20:50 -0500

    Running

--- SedovExplosion/Sedov.c
+++ SedovExplosion/Sedov.c
@@ -37,8 +37,8 @@ int indexx(int i, int j, int k);
 void __init__();
 FLOAT getDT(FLOAT dx);
 FLOAT*** allocate3d();
-void evolveU(float dt,float dx);
-void updateF();
+FLOAT evolve(float dt,float dx);
+
 
 //------------------------------------------------------------------------------
 //   GLOBAL VARIABLES
@@ -55,16 +55,13 @@ FLOAT blast;
 // Gas temperature in Kelvin
 FLOAT T;
 // Gas Pressure in Pascals
-FLOAT P;
+FLOAT p_ini;
 // Gas initial density
 FLOAT rho_ini;
 // Velocity v
 // Pressure p, internal energy e per unit mass, density rho, total energy E per unit volume
-FLOAT e;
-FLOAT**** v;
-FLOAT*** p;
-FLOAT*** E;
-FLOAT*** rho;
+FLOAT e_ini;
+
 /*
  * AUXILIAR VECTORS THAT FOLLOW WAVE EQ Ut + (d/dxi)Fi = 0
  *      | rho    |           |  rho*vi                   |                                        
@@ -83,47 +80,44 @@ FLOAT***** F;
 
 
 int main(int argc, char** argv){
+
 	// Temperature is 300K
 	T = 300.0;
 	// Initial gas density is 1kg/m3
 	rho_ini = 1.0;
 	// Pressure is atmospheric
-	P = 1.0*atm;
-	// Blast is defined
+	p_ini = 1.0*atm;
+	// Blast is defined J/kg
 	blast = pow(10.0,10.0);
-	// Internal energy depends on pressure and volume: e = cv*n*T/m = P*(V/m)*(cv/R) = P*cv/rho
-	e = P*pow(1.0*L,3.0)*cv/rho_ini;
-	// N is defined 
-	N = ((FLOAT)L)/((FLOAT)resol);
+	// Internal energy depends on pressure and volume: p = rho*e*(gamma-1)
+	e_ini = p_ini/(rho_ini*(gamma-1));
+	// N is defined (2 more for phantom) 
+	N = ((FLOAT)L)/((FLOAT)resol) + 2;
 	// Variables are allocated
 	allocateAll();
 	printf("aal iis weell (ALLOCATION)N=%d\n",N);
 	// Variables are initialised
 	__init__();
 	printf("aal iis weell (INITIALIZATION)\n");
-	   //
-	  //  TIME EVOLUTION
-	 //
+
+	   ///////////////////////////
+	  //  TIME EVOLUTION       //
+	 ///////////////////////////
+
 	// Time variable t, deltas in position and time: dx,dt
 	FLOAT time = 0;
 	FLOAT dx = resol;
-	FLOAT dt;
+	FLOAT dt = dx/sqrt(rho_ini*gamma*(gamma-1)*blast);
 	bool go_on = true;
+
 	do{	
-		// Gets stable time difference
-		dt = getDT(dx);
-		printf("aal iis weell (DT)\n");
-		// Evolves vector U
-		evolveU(dt,dx);
+		printf("%f,%f\n",time,dt);
+		// Evolves vector U, actualizes dt
+		dt = evolve(dt,dx);
 		printf("aal iis weell (EVOLVE)\n");
-		// Updates Fs in terms of U
-		updateF();
-		printf("aal iis weell (UPDATE)\n");
-		printf("%f\n",time);
+
 		time+=dt;
-		if(time > 1){
-			go_on = false;
-		}
+
 	} while(go_on);
 	return 0;
 }
@@ -139,16 +133,14 @@ int main(int argc, char** argv){
 
 
 /*
- * Allocates cubic nxnxn matrix into unidimentional FLOAT array arr
+ * Allocates cubic variables U and Fs
  *
  */
 void allocateAll(){
-	v = malloc(3*sizeof(FLOAT***));
 	U = malloc(5*sizeof(FLOAT***));
 	F = malloc(3*sizeof(FLOAT****));
 	int i,j;
 	for( i = 0; i < 3; i++ ){
-		v[i] = allocate3d();
 		F[i] = malloc(5*sizeof(FLOAT***));
 		for( j = 0; j < 5; j++ ){
 			F[i][j] = allocate3d();
@@ -157,9 +149,6 @@ void allocateAll(){
 	for( j = 0; j < 5; j++ ){
 		U[j] = allocate3d();
 	}	
-	p = allocate3d();
-	rho = allocate3d();
-	E = allocate3d();
 }
 
 /*
@@ -168,202 +157,170 @@ void allocateAll(){
  */
 void __init__(){
 
-	int i,j,k,l,m,n;
-	// Fills variables as if it was a normal ideal gas
-	for( i = 0; i < N; i++){
-		for( j = 0; j < N; j++){
-			for( k = 0; k < N; k++){
-				   p[i][j][k] = P;
-				v[0][i][j][k] = 0.0;
-				v[1][i][j][k] = 0.0;
-				v[2][i][j][k] = 0.0;
-				 rho[i][j][k] = rho_ini;
-				// Initial velocity is 0, so Total energy E is only internal
-				// E = rho(e+(1/2)*|v^2|)
-				E[i][j][k]    = rho_ini*e;
-				//printf("aal iis weell (ALLOCATION)%d,%d,%d\n",i,j,k);
-			}
-		}
-	}
-	//printf("aal iis weell (VELOCITIES,E,rho,p)\n");
-	// Sedovs blast energy is corrected for the blast (4m side)
-	E[N/2  ][N/2  ][N/2  ] = rho_ini*blast;
-	E[N/2-1][N/2  ][N/2  ] = rho_ini*blast;
-	E[N/2  ][N/2-1][N/2  ] = rho_ini*blast;
-	E[N/2  ][N/2  ][N/2-1] = rho_ini*blast;
-	E[N/2  ][N/2-1][N/2-1] = rho_ini*blast;
-	E[N/2-1][N/2  ][N/2-1] = rho_ini*blast;
-	E[N/2-1][N/2-1][N/2  ] = rho_ini*blast;
-	E[N/2-1][N/2-1][N/2-1] = rho_ini*blast;
+	int i,j,l,m,n;
+	// Initial velocity and Energy density are defined E = rho(e+0.5v**2)
+	FLOAT E_ini = rho_ini*e_ini;
+	FLOAT* v = malloc(3*sizeof(FLOAT));
+	v[0] = 0;
+	v[1] = 0;
+	v[2] = 0;
+
 	for( l = 0; l < N; l++){
 		for( m = 0; m < N; m++){
 			for( n = 0; n < N; n++){
+
 				// Defining F
 				for( j = 0; j < 3; j++ ){
-					F[j][0][l][m][n] = rho[l][m][n]*v[j][l][m][n];
+					F[j][0][l][m][n] = rho_ini*v[j];
 					for( i = 1; i < 4; i++ ){
-						F[j][i][l][m][n] = rho[l][m][n]*v[j][l][m][n]*v[i-1][l][m][n] + (j==(i-1))*p[l][m][n];
+						F[j][i][l][m][n] = rho_ini*v[j]*v[i-1] + (j==(i-1))*p_ini;
 						//delta_j,i-1 = (j==(i-1))
 					}
-					F[j][4][l][m][n] = (rho[l][m][n]*E[l][m][n]+p[l][m][n])*v[j][l][m][n];
+					F[j][4][l][m][n] = (rho_ini*E_ini+p_ini)*v[j];
 				}
+
 				// Defining U
-				U[0][l][m][n] = rho[l][m][n];
+				U[0][l][m][n] = rho_ini;
 				for( i = 1; i < 4; i++ ){
-						U[i][l][m][n] = rho[l][m][n]*v[i-1][l][m][n];
+						U[i][l][m][n] = rho_ini*v[i-1];
 				}
-				U[4][l][m][n] = rho[l][m][n]*E[l][m][n];
+				U[4][l][m][n] = rho_ini*E_ini;
+
 			}
 		}
 	}
+
+	// Corrects for the blast
+	E_ini = rho_ini*blast;
+	FLOAT p_blast =  (gamma-1)*blast;
+	// Defining F
+	for( j = 0; j < 3; j++ ){
+		for( i = 1; i < 4; i++ ){
+			F[j][i][N/2][N/2][N/2] = rho_ini*v[j]*v[i-1] + (j==(i-1))*p_blast;
+			//delta_j,i-1 = (j==(i-1))
+		}
+		F[j][4][N/2][N/2][N/2] = (rho_ini*E_ini+p_ini)*v[j];
+	}
+
+	U[4][N/2][N/2][N/2] = rho_ini*E_ini;
+	free(v);
 }
 
+
+
 /*
- * Evolves vector U given dx and dt according to the finite volume method
+ * Evolves vector U and actualizes vector F afterwards, given dx and dt according to the finite volume method
  * Finite volume is performed over a volume-centered cubic uniform grid
  * Volume integrals are approximated given variables constant over finite volumes
  * Surface integrals are approximated given variables constant over square surfaces
  * Values of variables on surfaces are calculated as the mean of the adjacent volumes
  * Boundary conditions are taken as free boundary :
  * (directional derivative perpendicular to the boundary surface vanishes) (This vanishes the respective surface integral)
+ * !!!RETURN!!! This method returns the time difference dt for the other time step
+ * delta in time is calculated according to the sound speed boundary which defines stability
  *
  */
-void evolveU(float dt,float dx){
+FLOAT evolve(float dt,float dx){
 	// Creates new array to avoid over-evolving
-	int i,j,k,l;
+	int i,j,k,l,m;
 	FLOAT**** U_new = malloc(5*sizeof(FLOAT***));
+
 	for( i = 0; i < 5; i++){
 		U_new[i] = allocate3d();
 	}
+
+	// Temporal variables to deduce maximum velocity and sound speed
+	FLOAT cmax = -1;
+	FLOAT vmax = -1;
+
+
+	  ////////////////////////////
+	 //   Evolution of Unew    //
+	////////////////////////////
+	
+
 	for( l = 0; l < 5; l++){
 		for( i = 1; i < N-1; i++){
 			for( j = 1; j < N-1; j++){
-				// CENTRAL VOLUMES		
 				for( k = 1; k < N-1; k++){
+
 					U_new[l][i][j][k]=U[l][i][j][k]-(0.5*dt/dx)*(F[0][l][i+1][j][k]+F[1][l][i][j+1][k]+F[2][l][i][j][k+1]
 					-F[0][l][i-1][j][k]-F[1][l][i][j-1][k]-F[2][l][i][j][k-1]);
+
 				}
-				// BOUNDARY CONDITIONS (EDGES of the cube)
-				//////////////////////////////////////////////// DOWN
-				k = 0;			
-				U_new[l][i][j][k]=U[l][i][j][k]-(0.5*dt/dx)*(F[0][l][i+1][j][k]+F[1][l][i][j+1][k]+F[2][l][i][j][k+1]
-				-F[0][l][i-1][j][k]-F[1][l][i][j-1][k]+(F[2][l][i][j][k]));//6
-				////////////////////////////////////////////////// UP				
-				k = N-1;
-				U_new[l][i][j][k]=U[l][i][j][k]-(0.5*dt/dx)*(F[0][l][i+1][j][k]+F[1][l][i][j+1][k]-(F[2][l][i][j][k])
-				-F[0][l][i-1][j][k]-F[1][l][i][j-1][k]-F[2][l][i][j][k-1]);//3
-				////////////////////////////////////////////////// SOUTH (INDICES ARE PERMUTATED)				
-				k = 0;
-				U_new[l][j][k][i]=U[l][j][k][i]-(0.5*dt/dx)*(F[0][l][j+1][k][i]+F[1][l][j][k+1][i]+F[2][l][j][k][i+1]
-				-F[0][l][j-1][k][i]+(F[1][l][j][k][i])-F[2][l][j][k][i-1]);//5
-				////////////////////////////////////////////////// NORTH			
-				k = N-1;
-				U_new[l][j][k][i]=U[l][j][k][i]-(0.5*dt/dx)*(F[0][l][j+1][k][i]-(F[1][l][j][k][i])+F[2][l][j][k][i+1]
-				-F[0][l][j-1][k][i]-F[1][l][j][k-1][i]-F[2][l][j][k][i-1]);//2				
-				////////////////////////////////////////////////// WEST			
-				k = 0;
-				U_new[l][k][i][j]=U[l][k][i][j]-(0.5*dt/dx)*(F[0][l][k+1][i][j]+F[1][l][k][i+1][j]+F[2][l][k][i][j+1]
-				+(F[0][l][k][i][j])-F[1][l][k][i-1][j]-F[2][l][k][i][j-1]);//4
-				////////////////////////////////////////////////// EAST			
-				k = N-1;
-				U_new[l][k][i][j]=U[l][k][i][j]-(0.5*dt/dx)*(-(F[0][l][k][i][j])+F[1][l][k][i+1][j]+F[2][l][k][i][j+1]
-				-F[0][l][k][i][j]-F[1][l][k][i-1][j]-F[2][l][k][i][j-1]);//1
 			}
-			// BOUNDARY CONDITIONS (EDGES of the cube)
-			//////////////////////////////////////////////// DOWN
-			k = 0;
-			j = 0;//SOUTH	
-			U_new[l][i][j][k]=U[l][i][j][k]-(0.5*dt/dx)*(F[0][l][i+1][j][k]+F[1][l][i][j+1][k]+F[2][l][i][j][k+1]
-			-F[0][l][i-1][j][k]+(F[1][l][i][j][k])+(F[2][l][i][j][k]));//6,5
-			j = N-1;//NORTH
-			U_new[l][i][j][k]=U[l][i][j][k]-(0.5*dt/dx)*(F[0][l][i+1][j][k]-(F[1][l][i][j][k])+F[2][l][i][j][k+1]
-			-F[0][l][i-1][j][k]-F[1][l][i][j-1][k]+(F[2][l][i][j][k]));//6,2
-			////////////////////////////////////////////////// UP				
-			k = N-1;
-			j = 0;// SOUTH
-			U_new[l][i][j][k]=U[l][i][j][k]-(0.5*dt/dx)*(F[0][l][i+1][j][k]+F[1][l][i][j+1][k]-(F[2][l][i][j][k])
-			-F[0][l][i-1][j][k]+(F[1][l][i][j][k])-F[2][l][i][j][k-1]);//3,5
-			j = N-1;// NORTH
-			U_new[l][i][j][k]=U[l][i][j][k]-(0.5*dt/dx)*(F[0][l][i+1][j][k]-(F[1][l][i][j][k])-(F[2][l][i][j][k])
-			-F[0][l][i-1][j][k]-F[1][l][i][j-1][k]-F[2][l][i][j][k-1]);//3,2
-			////////////////////////////////////////////////// SOUTH (INDICES ARE PERMUTATED)				
-			k = 0;
-			j = 0;// WEST
-			U_new[l][j][k][i]=U[l][j][k][i]-(0.5*dt/dx)*(F[0][l][j+1][k][i]+F[1][l][j][k+1][i]+F[2][l][j][k][i+1]
-			+(F[0][l][j][k][i])+(F[1][l][j][k][i])-F[2][l][j][k][i-1]);//5,4
-			j = N-1;// EAST
-			U_new[l][j][k][i]=U[l][j][k][i]-(0.5*dt/dx)*(-(F[0][l][j][k][i])+F[1][l][j][k+1][i]+F[2][l][j][k][i+1]
-			-F[0][l][j-1][k][i]+(F[1][l][j][k][i])-F[2][l][j][k][i-1]);//5,1
-			////////////////////////////////////////////////// NORTH			
-			k = N-1;
-			j = 0;// WEST
-			U_new[l][j][k][i]=U[l][j][k][i]-(0.5*dt/dx)*(F[0][l][j+1][k][i]-(F[1][l][j][k][i])+F[2][l][j][k][i+1]
-			+(F[0][l][j][k][i])-F[1][l][j][k-1][i]-F[2][l][j][k][i-1]);//2,4
-			j = N-1;// EAST	
-			U_new[l][j][k][i]=U[l][j][k][i]-(0.5*dt/dx)*(-(F[0][l][j][k][i])-(F[1][l][j][k][i])+F[2][l][j][k][i+1]
-			-F[0][l][j-1][k][i]-F[1][l][j][k-1][i]-F[2][l][j][k][i-1]);//2,1				
-			////////////////////////////////////////////////// WEST			
-			k = 0;
-			j = 0;// DOWN
-			U_new[l][k][i][j]=U[l][k][i][j]-(0.5*dt/dx)*(F[0][l][k+1][i][j]+F[1][l][k][i+1][j]+F[2][l][k][i][j+1]
-			+(F[0][l][k][i][j])-F[1][l][k][i-1][j]+(F[2][l][k][i][j]));//4,6
-			j = N-1;// UP
-			U_new[l][k][i][j]=U[l][k][i][j]-(0.5*dt/dx)*(F[0][l][k+1][i][j]+F[1][l][k][i+1][j]-(F[2][l][k][i][j])
-			+(F[0][l][k][i][j])-F[1][l][k][i-1][j]-F[2][l][k][i][j-1]);//4,3
-			////////////////////////////////////////////////// EAST			
-			k = N-1;
-			j = 0;// DOWN
-			U_new[l][k][i][j]=U[l][k][i][j]-(0.5*dt/dx)*(-(F[0][l][k][i][j])+F[1][l][k][i+1][j]+F[2][l][k][i][j+1]
-			-F[0][l][k][i][j]-F[1][l][k][i-1][j]+(F[2][l][k][i][j]));//1,6
-			j = N-1;// UP
-			U_new[l][k][i][j]=U[l][k][i][j]-(0.5*dt/dx)*(-(F[0][l][k][i][j])+F[1][l][k][i+1][j]-(F[2][l][k][i][j])
-			-F[0][l][k][i][j]-F[1][l][k][i-1][j]-F[2][l][k][i][j-1]);//1,3
-		}
-	// BOUNDARY CONDITIONS (CORNERS of the cube)
-	//////////////////////////////////////////////// DOWN
-	k = 0;
-	j = 0;/////////////////SOUTH
-	i = 0;//WEST
-	U_new[l][i][j][k]=U[l][i][j][k]-(0.5*dt/dx)*(F[0][l][i+1][j][k]+F[1][l][i][j+1][k]+F[2][l][i][j][k+1]
-	+(F[0][l][i][j][k])+(F[1][l][i][j][k])+(F[2][l][i][j][k]));//6,5,4
-	i = N-1;//EAST
-	U_new[l][i][j][k]=U[l][i][j][k]-(0.5*dt/dx)*(-(F[0][l][i][j][k])+F[1][l][i][j+1][k]+F[2][l][i][j][k+1]
-	-F[0][l][i-1][j][k]+(F[1][l][i][j][k])+(F[2][l][i][j][k]));//6,5,1
-	j = N-1;///////////////NORTH
-	i = 0  ;//WEST
-	U_new[l][i][j][k]=U[l][i][j][k]-(0.5*dt/dx)*(F[0][l][i+1][j][k]-(F[1][l][i][j][k])+F[2][l][i][j][k+1]
-	+(F[0][l][i][j][k])-F[1][l][i][j-1][k]+(F[2][l][i][j][k]));//6,2,4
-	i = N-1;//EAST
-	U_new[l][i][j][k]=U[l][i][j][k]-(0.5*dt/dx)*(-(F[0][l][i][j][k])-(F[1][l][i][j][k])+F[2][l][i][j][k+1]
-	-F[0][l][i-1][j][k]-F[1][l][i][j-1][k]+(F[2][l][i][j][k]));//6,2,1
-	////////////////////////////////////////////////// UP
-	k = N-1;
-	j = 0;//////////////////SOUTH
-	i = 0;//WEST
-	U_new[l][i][j][k]=U[l][i][j][k]-(0.5*dt/dx)*(F[0][l][i+1][j][k]+F[1][l][i][j+1][k]-(F[2][l][i][j][k])
-	+(F[0][l][i][j][k])+(F[1][l][i][j][k])-F[2][l][i][j][k-1]);//3,5,4
-	i = N-1;//EAST
-	U_new[l][i][j][k]=U[l][i][j][k]-(0.5*dt/dx)*(-(F[0][l][i][j][k])+F[1][l][i][j+1][k]-(F[2][l][i][j][k])
-	-F[0][l][i-1][j][k]+(F[1][l][i][j][k])-F[2][l][i][j][k-1]);//3,5,1
-	j = N-1;////////////////NORTH
-	i = 0  ;//WEST
-	U_new[l][i][j][k]=U[l][i][j][k]-(0.5*dt/dx)*(F[0][l][i+1][j][k]-(F[1][l][i][j][k])-(F[2][l][i][j][k])
-	+(F[0][l][i][j][k])-F[1][l][i][j-1][k]-F[2][l][i][j][k-1]);//3,2,4			
-	i = N-1;//EAST
-	U_new[l][i][j][k]=U[l][i][j][k]-(0.5*dt/dx)*(-(F[0][l][i][j][k])-(F[1][l][i][j][k])-(F[2][l][i][j][k])
-	-F[0][l][i-1][j][k]-F[1][l][i][j-1][k]-F[2][l][i][j][k-1]);//3,2,1				
+		}		
 	}
-	// Passes from U_new to U
-	for( l = 0; l < 5; l++){
-		for( i = 1; i < N-1; i++){
-			for( j = 1; j < N-1; j++){
-				for( k = 1; k < N-1; k++){
+
+
+	  //////////////////////////////////////////////////////////////////////////
+	 // Passes from U_new to U, Updates F and calculates maximum sound speed //
+	//////////////////////////////////////////////////////////////////////////
+
+ 
+	// Temporal variables
+	FLOAT rho,e,E,p;
+	FLOAT* v = malloc(3*sizeof(FLOAT));
+
+	for( i = 1; i < N-1; i++){
+		for( j = 1; j < N-1; j++){
+			for( k = 1; k < N-1; k++){
+				for( l = 0; l < 5; l++){
+
+					// U is actualized
 					U[l][i][j][k] = U_new[l][i][j][k];	
+
+				}
+
+				// From auxiliar variable U to usual variables rho,p,E,v
+				rho  = U[0][i][j][k];
+				v[0] = U[1][i][j][k]/rho;
+				v[1] = U[2][i][j][k]/rho;
+				v[2] = U[3][i][j][k]/rho;
+				E    = U[4][i][j][k];
+				e    = (E/rho) - 0.5*(pow(v[0],2)+pow(v[1],2)+pow(v[2],2));
+				p    = (gamma-1)*rho*e;
+
+				// Calculates sound speed and verifies maximum for sound speed and velocities
+				//FLOAT c_now = sqrt((gamma+1)*(E[i][j][k]+p[i][j][k]/rho[i][j][k])); DANGER
+				FLOAT c_now = sqrt((gamma+1)*(E+p));
+				if( c_now > cmax ){
+					cmax = c_now;
+				}
+				if( abs(v[0]) > vmax ){
+					vmax = abs(v[0]);
+				}
+				if( abs(v[1]) > vmax ){
+					vmax = abs(v[1]);
+				}
+				if( abs(v[2]) > vmax ){
+					vmax = abs(v[2]);
+				}
+	
+
+				// From usual variables to auxiliar Fi
+				for( l = 0; l < 3; l++ ){
+
+					F[l][0][i][j][k] = rho*v[l];
+
+					for( m = 1; m < 4; m++ ){
+
+						F[l][m][i][j][k] = rho*v[l]*v[m-1] + (l==(m-1))*p;
+
+					}
+
+					F[l][4][i][j][k] = (rho*E+p)*v[l];
 				}
 			}
 		}
 	}
-	// Free memory for U_new
+
+
+	  /////////////////////////////////
+	 // Free memory for U_new and v //
+	/////////////////////////////////
+
+
 	for( l = 0; l < 5; l++){	
 		for( i = 0; i < N; i++){
 			for( j = 0; j < N; j++){
@@ -374,36 +331,10 @@ void evolveU(float dt,float dx){
 		free(U_new[l]);
 	}
 	free(U_new);
-}
-
-
-void updateF(){
-	int i,j,k,l,m;
-	for( i = 0; i < N; i++){
-		for( j = 0; j < N; j++){
-			for( k = 0; k < N; k++){
-				// From auxiliar variable U to usual variables rho,p,E,v
-				rho[i][j][k]  = U[0][i][j][k];
-				v[0][i][j][k] = U[1][i][j][k]/rho[i][j][k];
-				v[1][i][j][k] = U[2][i][j][k]/rho[i][j][k];
-				v[2][i][j][k] = U[3][i][j][k]/rho[i][j][k];
-				E[i][j][k]    = U[4][i][j][k];
-				FLOAT e_temp  = (E[i][j][k]/rho[i][j][k]) - 0.5*(pow(v[0][i][j][k],2)+pow(v[1][i][j][k],2)+pow(v[2][i][j][k],2));
-				p[i][j][k]    = (gamma-1)*rho[i][j][k]*e_temp;
-				//printf("11\n");
-				// From usual variables to auxiliar Fi
-				for( l = 0; l < 3; l++ ){
-					F[l][0][i][j][k] = rho[i][j][k]*v[l][i][j][k];
-					for( m = 1; m < 4; m++ ){
-						F[l][m][i][j][k] = rho[i][j][k]*v[l][i][j][k]*v[m-1][i][j][k] + (l==(m-1))*p[i][j][k];
-						//delta_j,i-1 = (j==(i-1))
-					}
-					F[l][4][i][j][k] = (rho[i][j][k]*E[i][j][k]+p[i][j][k])*v[l][i][j][k];
-				}
-				//printf("12\n");
-			}
-		}
-	}
+	free(v);
+	
+	// RETURNS delta time
+	return dx/(vmax+cmax);
 }
 
 
@@ -423,38 +354,6 @@ FLOAT*** allocate3d(){
 	return vec;
 }
 
-/*
- * Gets Vectors delta in time according to the sound speed boundary which defines stability
- *
- */
-FLOAT getDT(float dx){
-	// Sound speed is calculated as sqrt((gamma+1)h) h = E + p/rho
-	// Calculates maximum sound speed and maximum velocity 
-	int i,j,k;
-	FLOAT cmax = -1;
-	FLOAT vmax = -1;
-	for( i = 0; i < N; i++){
-		for( j = 0; j < N; j++){
-			for( k = 0; k < N; k++){
-				FLOAT c_now = sqrt((gamma+1)*(E[i][j][k]+p[i][j][k]/rho[i][j][k]));
-				// Actualizes maximums if necessary
-				if( c_now > cmax ){
-					cmax = c_now;
-				}
-				if( abs(v[0][i][j][k]) > vmax ){
-					vmax = v[0][i][j][k];
-				}
-				if( abs(v[1][i][j][k]) > vmax ){
-					vmax = abs(v[1][i][j][k]);
-				}
-				if( abs(v[2][i][j][k]) > vmax ){
-					vmax = abs(v[2][i][j][k]);
-				}	
-			}
-		}
-	}
-	return dx/(vmax+cmax);
-}
 
 
 
